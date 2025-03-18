b30555c86770b81195941ffec12b56dcda39276d
Karl <karlcampbell100@gmail.com>
2025-03-18 11:34:38 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# WPM Measure
+
+## To Do
+
+### REACT
+[ ] Div to hold the paragrah
+[ ] Div to hold individual word
+[ ] Div to hold each letter
+
+#### SCSS
+[ ] 
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server:{
+    host:true,
+    allowedHosts:['9731-82-16-4-18.ngrok-free.app'],
+  }
+})
+
